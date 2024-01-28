ec9c5cb2e21b57654924fdf7d7864c84f56c6a0f
saemii-24 <hims2419@gmail.com>
2024-01-28 22:26:17 +0900

    Feat: 이미지 변경

--- src/component/Icon/Logo.tsx
+++ src/component/Icon/Logo.tsx
@@ -2,9 +2,8 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { useDispatch } from "react-redux";
-import { resetData, subBottomNavClick } from "redux/projectBgSlice";
+import { resetData } from "redux/projectBgSlice";
 import { useParams } from "react-router-dom";
-import cn from "classnames";
 
 const Logo = () => {
   const navigate = useNavigate();
@@ -19,103 +18,50 @@ const Logo = () => {
     idNum = 0;
   }
 
-  //subBottomNav를 클릭하면 useDispatch는 true가 되어야 한다.
-  //state초기화를 위해 사용
-
   return (
-    <StyledLogo className="logo">
-      <ul>
-        <li
-          onClick={() => {
-            navigate("/");
-            dispatch(resetData(idNum));
-          }}
+    <StyledLogo
+      onClick={() => {
+        navigate("/");
+        dispatch(resetData(idNum));
+      }}
+      className="logo"
+    >
+      <StyledLogoIcon>
+        <svg
+          width="25"
+          height="25"
+          viewBox="0 0 25 25"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
         >
-          <StyledLogoIcon>
-            <svg
-              width="25"
-              height="25"
-              viewBox="0 0 25 25"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <circle cx="12.876" cy="12.8344" r="11.5287" stroke="#2F2F2F" />
-            </svg>
-            <svg
-              width="25"
-              height="25"
-              viewBox="0 0 25 25"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              className="logoStar"
-            >
-              <path d="M12.9768 6.10449V20.2271" stroke="black" />
-              <path d="M20.0381 13.166L5.91553 13.166" stroke="black" />
-              <path d="M17.97 8.17285L7.98381 18.159" stroke="black" />
-              <path d="M17.97 18.1592L7.98382 8.17302" stroke="black" />
-            </svg>
-          </StyledLogoIcon>
-          <StyledLogoText>HOME</StyledLogoText>
-        </li>
-        <li
-          className={cn("nav__prev", {
-            white: Number(id) == 0 || Number(id) == 3 || Number(id) == 4,
-          })}
-          onClick={() => {
-            dispatch(subBottomNavClick(true));
-            setTimeout(() => {
-              if (Number(id) === 0) {
-                navigate(`/project/${5}`);
-              } else {
-                navigate(`/project/${Number(id) - 1}`);
-              }
-            }, 100);
-          }}
+          <circle cx="12.876" cy="12.8344" r="11.5287" stroke="#2F2F2F" />
+        </svg>
+        <svg
+          width="25"
+          height="25"
+          viewBox="0 0 25 25"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="logoStar"
         >
-          <StyledLogoText>PREV</StyledLogoText>
-        </li>
-        <li
-          className={cn("nav__next", {
-            white: Number(id) == 0 || Number(id) == 3 || Number(id) == 4,
-          })}
-          onClick={async () => {
-            dispatch(subBottomNavClick(true));
-            setTimeout(() => {
-              if (Number(id) === 5) {
-                navigate(`/project/${0}`);
-              } else {
-                navigate(`/project/${Number(id) + 1}`);
-              }
-            }, 100);
-          }}
-        >
-          <StyledLogoText>NEXT</StyledLogoText>
-        </li>
-      </ul>
+          <path d="M12.9768 6.10449V20.2271" stroke="black" />
+          <path d="M20.0381 13.166L5.91553 13.166" stroke="black" />
+          <path d="M17.97 8.17285L7.98381 18.159" stroke="black" />
+          <path d="M17.97 18.1592L7.98382 8.17302" stroke="black" />
+        </svg>
+      </StyledLogoIcon>
+      <StyledLogoText>HOME</StyledLogoText>
     </StyledLogo>
   );
 };
 
-const StyledLogo = styled.nav`
+const StyledLogo = styled.div`
+  display: flex;
+  gap: 12px;
+  align-items: center;
   margin-top: 8vh;
-  ul,
-  li {
-    gap: 12px;
-    list-style: none;
-    display: flex;
-    align-items: center;
-  }
-  li {
-    cursor: pointer;
-    width: fit-content;
-    &.white > h6 {
-      color: #f8f8f8;
-    }
-  }
-  .nav__prev {
-    margin-left: auto;
-    margin-right: 12px;
-  }
+  cursor: pointer;
+  width: fit-content;
 `;
 
 const StyledLogoIcon = styled.div`
