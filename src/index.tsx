0e5f3b88d4dbe8004dbf58fb540b1226fc022113
saemii-24 <hims2419@gmail.com>
2024-03-30 23:18:51 +0900

    Chore: package.json 수정

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <Provider store={store}>
-    <BrowserRouter basename="saemii_portfolio">
+    <BrowserRouter basename="/saemii_portfolio">
       <App />
     </BrowserRouter>
   </Provider>
